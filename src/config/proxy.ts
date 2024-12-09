331bc84106e27104b10e401ae99883d14dff5d6a
FateZero <490293266@qq.com>
2024-12-09 18:14:22 +0800

    update

--- src/config/proxy.ts
+++ src/config/proxy.ts
@@ -1,12 +1,12 @@
 export const proxy = {
 	'/dev/': {
-		target: 'http://127.0.0.1:8001',
+		target: 'http://127.0.0.1:8002',
 		changeOrigin: true,
 		rewrite: (path: string) => path.replace(/^\/dev/, '')
 	},
 
 	'/prod/': {
-		target: 'https://show.cool-admin.com',
+		target: '/',
 		changeOrigin: true,
 		rewrite: (path: string) => path.replace(/^\/prod/, '/api')
 	}
